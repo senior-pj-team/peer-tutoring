bebbdb8d72a20772896ac820977558050738ccba
Yehtutkhant <williamkhant4@gmail.com>
2025-04-18 18:38:45 +0700

    session-view layout file conflict resolved

--- src/app/(protected)/session-view/layout.tsx
+++ src/app/(protected)/session-view/layout.tsx
@@ -1,46 +1,11 @@
 import SessionHeader from "@/components/custom/session/session-header";
-<<<<<<< HEAD
-import React from "react"
-import Tabs from "@/components/custom/tabs"
-=======
 import React from "react";
 import Tabs from "@/components/custom/tabs";
->>>>>>> 99037ae (home page finished)
 import EnrollAction from "@/components/custom/session/enroll-action";
 import UpcomingAction from "@/components/custom/session/upcoming-action";
 import CompletedAction from "@/components/custom/session/completed-action";
 import ArchivedAction from "@/components/custom/session/archived-action";
 
-<<<<<<< HEAD
-
-const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  const tabs = [
-    { name: 'Content', path: '/session-view/content' },
-    { name: 'Tutor Info', path: '/session-view/tutor-info' },
-    { name: 'Payment', path: '/session-view/payment-info' }
-  ];
-  return (
-    <div>
-      <SessionHeader />
-      <hr />
-      <div className="px-6 pb-10">
-        <Tabs tabs={tabs} />
-        <div className="relative">
-          {children}
-          <hr className="block xl:hidden"/>
-          <div className="static xl:absolute xl:top-15 xl:right-30 xl:shadow xl:border xl:shadow-sm xl:max-w-sm p-5 space-y-2">
-            
-            {/* Action card will be different according to what route it came from */}
-            {/* <EnrollAction/> */}
-            {/* <UpcomingAction/> */}
-            {/* <CompletedAction/> */}
-            {/* <ArchivedAction/> */}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-=======
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 	const tabs = [
 		{ name: "Content", path: "/session-view/content" },
@@ -58,16 +23,15 @@ const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 					<hr className="block xl:hidden" />
 					<div className="static xl:absolute xl:top-15 xl:right-30 xl:shadow xl:border xl:shadow-sm xl:max-w-sm p-5 space-y-2">
 						{/* Action card will be different according to what route it came from */}
-						{/* <EnrollAction /> */}
-						{/* <UpcomingAction /> */}
-						{/* <CompletedAction /> */}
-						<ArchivedAction />
+						{/* <EnrollAction/> */}
+						{/* <UpcomingAction/> */}
+						{/* <CompletedAction/> */}
+						{/* <ArchivedAction/> */}
 					</div>
 				</div>
 			</div>
 		</div>
 	);
->>>>>>> 99037ae (home page finished)
 };
 
 export default layout;
