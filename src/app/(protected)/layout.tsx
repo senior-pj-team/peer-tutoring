c8052c945a1ed48d787b821c1c1dc37d11ede86c
Aung Phyo Linn <aungphyolinn289@gmail.com>
2025-04-18 18:54:24 +0700

    Footer Added

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,15 +1,17 @@
 import Navbar from "@/components/custom/navbar/navbar";
+import { Footer } from "@/components/custom/footer";
 import React from "react";
 
 export default function Layout({
-	children,
+  children,
 }: Readonly<{ children: React.ReactNode }>) {
-	return (
-		<>
-			<Navbar />
-			<div className="overflow-auto pt-[5rem]">
-				{children}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen flex flex-col pt-[5rem]">
+        <div className="flex-grow overflow-auto">{children}</div>
+        <Footer />
+      </div>
+    </>
+  );
 }
---
+++ src/components/custom/footer.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Link from "next/link";
+
+export function Footer() {
+  return (
+    <footer className="footer bg-gray-900 text-gray-300 flex flex-col items-center gap-6 p-7 border-t-1 border-gray-600">
+      <div className="flex items-center gap-2 text-2xl font-semibold">
+        <span>O r i o n</span>
+      </div>
+      <aside>
+        <p className="text-lg">
+          © {new Date().getFullYear()} Orion Group. All rights reserved.
+        </p>
+      </aside>
+
+      <nav className="text-gray-300 grid grid-cols-2 sm:grid-cols-4 gap-8">
+        <Link href="#" className="link link-hover hover:text-gray-400">
+          License
+        </Link>
+        <Link href="#" className="link link-hover hover:text-gray-400">
+          Help
+        </Link>
+        <Link href="#" className="link link-hover hover:text-gray-400">
+          Contact
+        </Link>
+        <Link href="#" className="link link-hover hover:text-gray-400">
+          Policy
+        </Link>
+      </nav>
+
+      <div className="flex gap-6 mt-6">
+        <a
+          href="#"
+          className="link text-gray-300 hover:text-gray-400"
+          aria-label="Github Link"
+        >
+          <span className="icon-[tabler--brand-github] size-5"></span>
+        </a>
+        <a
+          href="#"
+          className="link text-gray-300 hover:text-gray-400"
+          aria-label="Facebook Link"
+        >
+          <span className="icon-[tabler--brand-facebook] size-5"></span>
+        </a>
+        <a
+          href="#"
+          className="link text-gray-300 hover:text-gray-400"
+          aria-label="Twitter Link"
+        >
+          <span className="icon-[tabler--brand-x] size-5"></span>
+        </a>
+        <a
+          href="#"
+          className="link text-gray-300 hover:text-gray-400"
+          aria-label="Google Link"
+        >
+          <span className="icon-[tabler--brand-google] size-5"></span>
+        </a>
+      </div>
+    </footer>
+  );
+}
