90027534e54b01d2b43a439006858bdd27f73a40
NyiWaiYanHtoon <nwai39771@gmail.com>
2025-04-26 18:49:27 +0700

    Finalize Become tutor page

--- src/app/(protected)/(student)/(footer)/become-tutor/page.tsx
+++ src/app/(protected)/(student)/(footer)/become-tutor/page.tsx
@@ -3,7 +3,7 @@ import Stepper from "@/components/custom/tutor-registration/Stepper";
 
 export default function BecomeTutorPage() {
   return (
-    <div className="max-w-2xl mx-auto p-4 font-sans">
+    <div className="max-w-2xl mx-auto p-4">
       <Stepper />
     </div>
   );
